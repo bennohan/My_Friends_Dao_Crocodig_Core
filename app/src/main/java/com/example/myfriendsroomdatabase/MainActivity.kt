b4c1198e5f7554cbd4d5145a3ba36f4466d9710a
bennohan <bebenbarnet@gmail.com>
2022-11-04 21:35:14 +0700

    Dengan Core Dao 1

--- app/src/main/java/com/example/myfriendsroomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/myfriendsroomdatabase/MainActivity.kt
@@ -11,16 +11,22 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var myDatabase: MyDatabase
+    private lateinit var repository: FriendRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         binding.activity = this
         myDatabase = MyDatabase.getDatabase(this)
+        repository = FriendRepository(myDatabase.friendDao())
 
-        myDatabase.friendDao().getAll().observe(this){
+
+        repository.getAll().observe(this){
             binding.rvFriend.adapter = FriendAdapter(it)
         }
+        //myDatabase.friendDao().getAll().observe(this){
+            //binding.rvFriend.adapter = FriendAdapter(it)
+        //}
     }
 
     fun addFriend(){
